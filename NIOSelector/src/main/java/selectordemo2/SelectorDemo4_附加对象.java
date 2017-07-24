90c71e3666779a53650fd24b2c31cf06c1b23aef
holdbelief <81971357@qq.com>
2017-07-24 16:27:29 +0800

    附件对象 和 当有channel好的时候的例子

--- NIOSelector/src/main/java/selectordemo2/SelectorDemo4_附加对象.java
+++ NIOSelector/src/main/java/selectordemo2/SelectorDemo4_附加对象.java
@@ -7,6 +7,8 @@ import java.nio.channels.FileChannel;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
+import java.util.Iterator;
+import java.util.Set;
 
 public class SelectorDemo4_附加对象 {
 
@@ -23,6 +25,30 @@ public class SelectorDemo4_附加对象 {
 		
 		BeAttachment attachment2 = new BeAttachment();
 		SelectionKey key = serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT, attachment2);
+	
+		while ( true ) {
+			int readyChannels = selector.select();
+			if ( readyChannels == 0 ) continue;
+			
+			Set<SelectionKey> selectedKeys = selector.selectedKeys();
+			Iterator<SelectionKey> keyIterator = selectedKeys.iterator();
+			
+			while ( keyIterator.hasNext() ) {
+				SelectionKey temp_key = keyIterator.next();
+				
+				if ( temp_key.isAcceptable() ) {
+					// a connection was accepted by a ServerSocketChannel.
+				} else if ( temp_key.isConnectable() ) {
+					// a connection was established with a remote server.
+				} else if ( temp_key.isReadable() ) {
+					// a channel is ready for reading
+				} else if ( temp_key.isWritable() ) {
+					// a channel is ready for wirting
+				}
+				
+				keyIterator.remove();
+			}
+		}
 	}
 
 }
